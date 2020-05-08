455608a9ddd7a7ab8d9a7c75fb85005b99acfad4
Saša Kajfeš <skajfes@gmail.com>
2020-05-08 15:34:26 +0200

    first days

---
+++ 2015/Day03/Program.fs
@@ -0,0 +1,53 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let getPath input =
+    let path =
+        Seq.mapFold (fun (x, y) dir ->
+            let h =
+                match dir with
+                | '<' -> (x - 1, y)
+                | '>' -> (x + 1, y)
+                | '^' -> (x, y - 1)
+                | 'v' -> (x, y + 1)
+                | _ -> failwith "invalid command"
+            (h, h)) (0, 0) input
+
+    (0, 0) :: ( fst path |> Seq.toList)
+    
+let distinctHouses path =
+    path
+    |> List.distinct
+    |> List.length
+    
+let splitInput input =
+    let indexed = Seq.indexed input
+    
+    let santaPath =
+        indexed
+        |> Seq.filter (fun (i, _) -> i % 2 = 0)
+        |> Seq.map snd
+    
+    let robotPath =
+        indexed
+        |> Seq.filter (fun (i, _) -> i % 2 = 1)
+        |> Seq.map snd
+    
+    (santaPath, robotPath)
+    
+let joinPath (santa, robot) =
+    List.append (getPath santa) (getPath robot)
+    
+
+[<EntryPoint>]
+let main argv =
+
+    let input = IO.File.ReadAllText("input.txt")
+
+    input |> getPath |> distinctHouses |> printfn "Part1: number of houses visited: %A"
+    input |> splitInput |> joinPath |> distinctHouses |> printfn "Part2: number of houses with robot: %A"
+    
+
+//    houses "^v^v^v^v^v" |> printfn "%A"
+    0 // return an integer exit code
---
+++ 2015/Day05/Program.fs
@@ -0,0 +1,47 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Text.RegularExpressions
+
+type NaughtyOrNice = Naughty | Nice
+
+let naughtyOrNice (s: string) =
+    let vowelCount = Seq.filter (fun c -> Seq.contains c "aeiou") s |> Seq.length
+    if vowelCount < 3 then Naughty else
+        
+    let hasDoubles = Seq.pairwise s |> Seq.exists (fun (a, b) -> a = b)
+    if hasDoubles = false then Naughty else
+        
+    let hasForbiddenStrings = Seq.map (fun t -> s.Contains((string)t)) ["ab"; "cd"; "pq"; "xy"] |> Seq.exists ((=)true) 
+    if hasForbiddenStrings then Naughty else Nice
+    
+let naughtyOrTwice (s: string) =
+    let hasPair = Regex.IsMatch(s, "(..).*\1")
+    let hasRepeatingLetter = Regex.IsMatch(s, "(.).\1")
+    if hasPair && hasRepeatingLetter then Nice else Naughty
+
+let howManyNice f input =
+    input
+    |> Seq.map f
+    |> Seq.filter ((=) Nice)
+    |> Seq.length
+    
+[<EntryPoint>]
+let main argv =
+    // tests
+    //    naughtyOrNice "ugknbfddgicrmopn" |> printfn "ugknbfddgicrmopn %A"
+    //    naughtyOrNice "aaa" |> printfn "aaa %A"
+    //    naughtyOrNice "jchzalrnumimnmhp" |> printfn "jchzalrnumimnmhp %A"
+    //    naughtyOrNice "haegwjzuvuyypxyu" |> printfn "haegwjzuvuyypxyu %A"
+    //    naughtyOrNice "dvszwmarrgswjxmb" |> printfn "dvszwmarrgswjxmb %A"
+    
+    naughtyOrTwice "qjhvhtzxzqqjkmpb" |> printfn "qjhvhtzxzqqjkmpb %A"
+    naughtyOrTwice "xxyxx"|> printfn "xxyxx %A"
+    naughtyOrTwice "uurcxstgmygtbstg"|> printfn "uurcxstgmygtbstg %A"
+    naughtyOrTwice "ieodomkazucvgmuy"|> printfn "ieodomkazucvgmuy %A"
+    
+    let input = IO.File.ReadAllLines("input.txt")
+    printfn "Part1: nice strings %d" (howManyNice naughtyOrNice input)
+    printfn "Part2: nice strings %d" (howManyNice naughtyOrTwice input)
+    
+    0 // return an integer exit code
