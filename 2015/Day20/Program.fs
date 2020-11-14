dbbc85316e9e6a16bd761ec0a2b4089dcd21de51
Saša Kajfeš <skajfes@gmail.com>
2020-11-14 23:36:10 +0100

    day 20

---
+++ 2015/Day20/Program.fs
@@ -0,0 +1,29 @@
+﻿
+let sumOfDivisors n filter =
+    let max = float n |> sqrt |> int
+    [1..max]
+    |> List.filter (fun x -> n % x = 0)
+    |> List.collect (fun x -> [x; n/x])
+    |> List.distinct
+    |> List.filter filter
+    |> List.sum
+    
+let presentsForHouse n =
+    sumOfDivisors n (fun _ -> true)
+    |> (*)10
+
+let presentsForHouse' n =
+    sumOfDivisors n (fun x -> x * 50 >= n)
+    |> (*)11
+    
+let findHouseWithPresents target f =
+    Seq.unfold (fun n -> Some (n+1, n+1)) 1
+    |> Seq.filter (fun x -> f x > target)
+    |> Seq.head
+    
+[<EntryPoint>]
+let main argv =
+    findHouseWithPresents 29000000 presentsForHouse |> printfn "Part 1: %A"
+    findHouseWithPresents 29000000 presentsForHouse' |> printfn "Part 2: %A"
+    
+    0 // return an integer exit code
