5ca81815cb43a8326b5eef56fd5ac585c456abd1
Saša Kajfeš <skajfes@gmail.com>
2020-11-29 14:00:48 +0100

    script for new projects

---
+++ new.ps1
@@ -0,0 +1,17 @@
+
+param(
+	[Parameter(Mandatory=$true)]
+	[int]$Year,
+	[Parameter(Mandatory=$true)]
+	[int]$Day
+)
+
+if (-not (Test-Path $Year)) {
+	dotnet new sln -n "AdventOfCode_$Year" -o $Year
+}
+
+pushd $Year
+$p = "Day$($Day.ToString("00"))"
+dotnet new console -lang "F#" -o $p
+dotnet sln add $p/$p.fsproj
+popd
