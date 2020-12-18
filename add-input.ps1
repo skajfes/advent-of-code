872592e28822ee27c400af1529f6d483ae2bdb7c
Saša Kajfeš <skajfes@gmail.com>
2020-12-18 16:38:41 +0100

    Day18 2020

--- add-input.ps1
+++ add-input.ps1
@@ -15,16 +15,22 @@ $p = "Day$($Day.ToString("00"))"
 curl -HGET https://adventofcode.com/$Year/day/$Day/input -H "Cookie: session=$session" > $Year\$p\input.txt
 
 $name = Get-ChildItem $Year\$p\$p.fsproj | %{ $_.FullName }
-$file = New-Object XML
-$file.Load($name)
-$content = $file.CreateElement("Content")
-$includeAttr = $file.CreateAttribute("Include")
-$includeAttr.Value = "input.txt"
-$copy = $file.CreateElement("CopyToOutputDirectory")
-$copy.InnerText = "PreserveNewest"
-$content.AppendChild($copy)
-$content.Attributes.Append($includeAttr)
-$file.Project.ItemGroup.AppendChild($content)
-$file.save($name)
+
+if ((sls -Path $name -Pattern "input.txt").Length -eq 0) {
+	$file = New-Object XML
+	$file.Load($name)
+	$content = $file.CreateElement("Content")
+	$includeAttr = $file.CreateAttribute("Include")
+	$includeAttr.Value = "input.txt"
+	$copy = $file.CreateElement("CopyToOutputDirectory")
+	$copy.InnerText = "PreserveNewest"
+	$content.AppendChild($copy)
+	$content.Attributes.Append($includeAttr)
+	$file.Project.ItemGroup.AppendChild($content)
+	$file.save($name)
+}
+
+cat $name
+cat $Year\$p\input.txt
 
 popd
