f1396fb90d69c18c5297584f8c50f0fd8a727f0c
Saša Kajfeš <skajfes@gmail.com>
2020-12-25 12:57:39 +0100

    Day24 speed up

--- 2020/Day24/Program.fs
+++ 2020/Day24/Program.fs
@@ -1,6 +1,5 @@
-
-
 open System.Diagnostics
+
 let (|Prefix|_|) (prefix: string) (input: string) =
     if input.StartsWith(prefix) then Some <| input.[prefix.Length..]
     else None
@@ -30,58 +29,58 @@ let flip start instruction =
         | NE -> (r - 1, c + 0)
         | NW -> (r - 1, c - 1))
 
-
 let flipAll instructions =
     instructions
     |> Array.map (flip (0, 0))
     |> Array.countBy id
     |> Array.filter (fun t -> snd t % 2 = 1)
     |> Array.map fst
+    |> Set.ofArray
 
 let countBlackTiles instructions =
     flipAll instructions
-    |> Array.length
+    |> Set.count
 
-let neighbourTiles (r, c) =
-    // [|[NE];[NW];[SE];[SW];[E];[W]|]|> Array.map (flip t)
-    [|(r-1, c); (r - 1, c - 1);  (r, c - 1); (r, c + 1); (r + 1, c); (r + 1, c + 1)|]
+let foldi (f: 'b -> 'a -> 'b) (init: 'b) (data: 'a[,]) =
+    let mutable res = init
+    for i in [0..Array2D.length1 data - 1] do
+        for j in [0..Array2D.length2 data - 1] do
+            res <- f res (data.[i,j])
+    res
+
+let printMap tiles =
+    let mutable res = ""
+    for i in [0..Array2D.length1 tiles - 1] do
+        for j in [0..Array2D.length2 tiles - 1] do
+            res <- res + if tiles.[i,j] then "#" else " "
+        res <- res + "\n"
+    System.Console.SetCursorPosition(0, 0)
+    printfn "%s" res
 
 let gameOfLife tiles =
 
-    let round tiles =
-        let ns =
-            tiles
-            |> Array.map (fun t -> t, neighbourTiles t)
-
-        let blacks =
-            ns
-            |> Array.filter (fun (t, ns) ->
-                let black_ns =
-                    ns
-                    |> Array.map (fun t -> Array.contains t tiles)
-                    |> Array.filter ((=)true)
-                    |> Array.length
-                match black_ns with
-                | 1 | 2 -> true
+    let round (tiles: bool[,]) =
+        // printMap tiles
+        tiles
+        |> Array2D.mapi (fun i j t ->
+            // don't check edges of array
+            if i = 0 || j = 0 || i = (Array2D.length1 tiles - 1) || j = (Array2D.length2 tiles - 1) then false else
+            // all neighbours
+            [|(i-1, j); (i - 1, j - 1);  (i, j - 1); (i, j + 1); (i + 1, j); (i + 1, j + 1)|]
+            |> Array.filter (fun (i, j) -> tiles.[i, j])
+            |> Array.length
+            |> fun l ->
+                match t, l with
+                | true, l when l = 1 || l = 2 -> true
+                | false, 2 -> true
                 | _ -> false)
-            |> Array.map fst
-
-        let newBlacks =
-            ns
-            |> Array.collect snd
-            |> Array.filter (fun t -> Array.contains t tiles |> not)
-            |> Array.countBy id
-            |> Array.filter (fun (t, count) ->
-                match count with
-                | 2 -> true
-                | _ -> false)
-            |> Array.map fst
-
-        Array.append blacks newBlacks
 
     (tiles, [1..100])
     ||> List.fold (fun ts r -> round ts)
-    |> Array.length
+    |> foldi (fun sum c -> sum + if c then 1 else 0) 0
+
+let createMap blacks =
+    Array2D.init 150 150 (fun i j -> Set.contains (i-75, j-75) blacks)
 
 [<EntryPoint>]
 let main argv =
@@ -92,7 +91,7 @@ let main argv =
     input |> parse |> countBlackTiles |> printfn "Part 1: %d"
     let p1 = sw.ElapsedMilliseconds
     sw.Restart()
-    input |> parse |> flipAll |> gameOfLife |> printfn "Part 2: %d"
+    input |> parse |> flipAll |> createMap |> gameOfLife |> printfn "Part 2: %d"
     let p2 = sw.ElapsedMilliseconds
     printfn "Part1: %d ms, Part2: %d ms" p1 p2
     
