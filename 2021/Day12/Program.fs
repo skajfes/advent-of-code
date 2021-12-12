6f17695c208995be20421eda73b6843a3fea382d
Saša Kajfeš <skajfes@gmail.com>
2021-12-12 13:47:11 +0100

    2021 Day12 speedup

--- 2021/Day12/Program.fs
+++ 2021/Day12/Program.fs
@@ -1,16 +1,11 @@
 open System.IO
-open System.Threading
 
 type Cave =
-    // | Start
-    // | End
     | Big of string
     | Small of string
 
 let cave (s: string) =
     match s with
-    // | "start" -> Small "start"
-    // | "end" -> Small "end"
     | s when System.Char.IsUpper(s[0]) -> Big s
     | s -> Small s
 
@@ -18,71 +13,56 @@ let parse (input: string[]) =
     input
     |> Array.map (fun line ->
         let [|a; b|] = line.Split("-")
-        (cave a, cave b)
+        [(cave a, cave b); (cave b, cave a)]
         )
     |> Array.toList
+    |> List.concat
+    |> List.groupBy fst
+    |> List.map (fun (a, caves) -> a, caves |> List.map snd)
+    |> Map.ofList
 
-let paths start caves =
-    caves
-    |> List.filter (fun (a, b) -> a = start || b = start)
+let paths start (caves: Map<Cave, Cave list>) =
+    caves[start]
+
+let print (path: Cave list) =
+    path
+    |> List.rev
     |> List.map (function
-                  | a, b when a = start -> b
-                  | a, b -> a)
-
-let rec traverse (start :: path) caves =
-    match start with
-    | Small "end" -> [start :: path]
-    | _ ->
-    paths start caves
-    |> List.filter (fun cave ->
-        match cave with
-        | Small c -> List.contains cave path |> not
-        | _ -> true)
-    |> List.map (fun p -> p::start::path)
-    |> List.collect (fun path -> traverse path caves)
-
-let isSmall cave =
-    match cave with
-    | Small _ -> true
-    | _ -> false
-
-let rec traverse2 (start :: path) caves =
-    match start with
-    | Small "end" -> [start :: path]
-    | _ ->
-    paths start caves
-    |> List.filter (fun cave ->
-        match cave with
-        | Small "start" -> false
-        | Small _ ->
-            match List.contains cave path with
-            | false -> true
-            | true ->
-                let anyDouble = start::path |> List.filter isSmall |> List.countBy id |> List.exists (fun (c, cnt) -> cnt >= 2)
-                not anyDouble
-        | _ -> true)
-    |> List.map (fun p -> p::start::path)
-    |> List.collect (fun path -> traverse2 path caves)
-
-let print (paths: Cave list list) =
-    paths
-    |> List.map (fun path -> path |> List.rev |> List.map (function | Small c -> c | Big c -> c) |> String.concat " -> ")
-    |> String.concat "\n"
+                | Small c -> c
+                | Big c -> c)
+    |> String.concat " -> "
     |> printfn "%s"
 
-    printfn "%d" (List.length paths)
+let rec traverse (start :: path) used caves =
+    match start with
+    | Small "end" ->
+        // print (start::path)
+        1
+    | _ ->
+        paths start caves
+        |> List.map (fun cave ->
+            match cave, used with
+            | Small "start", _ -> cave, false, used
+            | Small _, Some u ->
+                match List.contains cave path with
+                | false -> cave, true, used
+                | true -> cave, not u, Some true
+            | Small _, None ->
+                match List.contains cave path with
+                | false -> cave, true, None
+                | true -> cave, false, None
+            | _ -> cave, true, used)
+        |> List.filter (fun (_, a, _) -> a)
+        |> List.sumBy (fun (cave, _, used) -> traverse (cave::start::path) used caves)
+
 
 let part1 caves =
     caves
-    |> traverse [Small "start"]
-    // |> print
-    |> List.length
+    |> traverse [Small "start"] None
 
 let part2 caves =
     caves
-    |> traverse2 [Small "start"]
-    // |> print
-    |> List.length
+    |> traverse [Small "start"] (Some false)
 
 [<EntryPoint>]
 let main argv =
@@ -91,9 +71,9 @@ let main argv =
     let testInput = File.ReadAllLines("sample2.txt")
 
     // testInput |> parse |> part1 |> printfn "%A"
-    testInput |> parse |> part2 |> printfn "%A"
+    // testInput |> parse |> part2 |> printfn "%A"
 
-    // input |> parse |> part1 |> printfn "Part 1: %A"
+    input |> parse |> part1 |> printfn "Part 1: %A"
     input |> parse |> part2 |> printfn "Part 2: %A"
 
     0 // return an integer exit code
