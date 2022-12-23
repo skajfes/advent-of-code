19f1b3c6df8135f44403000077ae60ff0969b90c
Saša Kajfeš <skajfes@gmail.com>
2022-12-23 14:40:42 +0100

    2022 Day15 - make it fast

--- 2022/Day15/Program.fs
+++ 2022/Day15/Program.fs
@@ -1,4 +1,3 @@
-open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 
@@ -18,105 +17,145 @@ let parseLine line =
 
 let parse (input: string[]) = input |> Array.map parseLine
 
-let distance_points = Dictionary<int, (int*int)[]>()
-let pointsAtDistance distance =
-    if distance_points.ContainsKey(distance) then distance_points[distance] else
-    let points =
-        [ for dx in 0..distance -> dx, distance - dx ]
-        |> List.append [ for dx in 0..distance -> -dx, -1 * (distance - dx) ]
-        |> List.append [ for dx in 0..distance -> dx, -1 * (distance - dx) ]
-        |> List.append [ for dx in 0..distance -> -dx, (distance - dx) ]
-        |> List.distinct
-        |> List.toArray
-    distance_points[distance] <- points
-    points
-
-
-let exclusion_zones = Dictionary<int, (int * int)[]>()
-
-let exclusion_zone_deltas distance =
-    if exclusion_zones.ContainsKey(distance) then
-        exclusion_zones[distance]
-    else
-        let deltas = [| 0..distance |] |> Array.collect pointsAtDistance |> Array.distinct
-
-        exclusion_zones[distance] <- deltas
-        deltas
-
-let exclusion_zone (sensor, beacon) =
-    let distance = abs (fst sensor - fst beacon) + abs (snd sensor - snd beacon)
-
-    exclusion_zone_deltas distance
-    |> Array.map (fun (dx, dy) -> (fst sensor + dx, snd sensor + dy))
-
 let exclusion_row row ((sx, sy), (bx, by)) =
     let distance = abs (sx - bx) + abs (sy - by)
     let remaining = distance - abs (sy - row)
 
-    [| - remaining .. remaining |]
-    |> Array.map (fun x -> sx + x, row)
-    |> Array.filter ((<>)(bx, by))
+    if remaining < 0 then
+        None
+    else
+        Some(sx - remaining, sx + remaining)
 
+
+let rec removeBeacons beacons ranges =
+    match beacons, ranges with
+    | [], _ -> ranges
+    | _, [] -> []
+    | b :: bs, (xmin, xmax) :: _ when b > xmax -> removeBeacons bs ranges
+    | b :: bs, (xmin, xmax) :: rest when b < xmin -> (xmin, xmax) :: removeBeacons (b :: bs) rest
+    | b :: bs, (xmin, xmax) :: rest when b = xmin && xmin = xmax -> removeBeacons bs rest
+    | b :: bs, (xmin, xmax) :: rest when b = xmin -> removeBeacons bs ((xmin + 1, xmax) :: rest)
+    | b :: bs, (xmin, xmax) :: rest when b = xmax -> removeBeacons bs ((xmin, xmax - 1) :: rest)
+    | b :: bs, (xmin, xmax) :: rest -> removeBeacons bs ((xmin, b - 1) :: (b + 1, xmax) :: rest)
 
 let part1 row sensors =
+    let beacons =
+        sensors
+        |> Array.map snd
+        |> Array.filter (snd >> (=) row)
+        |> Array.map fst
+        |> Array.sort
+        |> Array.toList
+
     sensors
-    |> Array.collect (exclusion_row row)
-    |> Array.filter (fun (x, y) -> y = row)
-    |> Array.distinct
-    |> Array.length
+    |> Array.map (exclusion_row row)
+    |> Array.filter ((<>) None)
+    |> Array.map (function
+        | Some x -> x
+        | _ -> failwith "oops")
+    |> Array.sortBy fst
+    |> Array.fold
+        (fun ranges range ->
+            match ranges with
+            | [] -> [ range ]
+            | (rmin, rmax) :: ranges when rmax < fst range -> range :: (rmin, rmax) :: ranges
+            | (rmin, rmax) :: ranges when rmin <= fst range && rmax >= snd range -> (rmin, rmax) :: ranges
+            | (rmin, rmax) :: ranges -> (rmin, snd range) :: ranges)
+        []
+    |> removeBeacons beacons
+    |> List.map (fun (min, max) -> max - min + 1)
+    |> List.sum
 
-let part2 limit sensors =
-    let excluded =
-        sensors
-        |> Array.collect exclusion_zone
 
-    [|0..limit|]
-    |> Array.map (fun row -> excluded |> Array.filter (fun (x, y) -> y = row && x >= 0 && x <= limit))
-    |> Array.mapi (fun r row ->
-        [|0..limit|]
-        |> Array.filter (fun x -> row |> Array.contains (x, r) |> not)
-        |> Array.map (fun x -> x, r)
-    )
+let toLines ((sx, sy), (bx, by)) =
+    let distance = abs (sx - bx) + abs (sy - by) + 1 // manhattan distance + 1 to get the points just outside sensor reach
+
+    let point_up = (sx, sy + distance)
+    let point_down = (sx, sy - distance)
+    let point_left = (sx - distance, sy)
+    let point_right = (sx + distance, sy)
+
+    let line_1 = (1, sy - sx + distance)
+    let line_2 = (1, sy - sx - distance)
+    let line_3 = (-1, sy + sx - distance)
+    let line_4 = (-1, sy + sx + distance)
+
+    [| (point_left, point_up), line_1
+       (point_down, point_right), line_2
+       (point_left, point_down), line_3
+       (point_up, point_right), line_4 |]
+
+let areParallel (_, (a1, _)) (_, (a2, _)) = a1 = a2
+
+let xOutOfRange (((ax1, _), (ax2, _)), _) (((bx1, _), (bx2, _)), _) =
+    (ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) |> not
+
+let intersect (((ax1, ay1), (ax2, ay2)), (aa, ab)) (((bx1, by1), (bx2, by2)), (ba, bb)) =
+    let xd = decimal (bb - ab) / decimal (aa - ba)
+    let x = int xd
+
+    if xd <> round xd then
+        None
+    elif x < ax1 || x > ax2 || x < bx1 || x > bx2 then
+        None
+    else
+        let x = int x
+        let y = aa * x + ab
+
+        let [ ay1; ay2 ] = [ ay1; ay2 ] |> List.sort
+        let [ by1; by2 ] = [ by1; by2 ] |> List.sort
+
+        if y < ay1 || y > ay2 || y < by1 || y > by2 then
+            None
+        else
+            Some(x, y)
+
+let intersectWithSensor size sa sb =
+    Array.allPairs sa sb
+    |> Array.map (fun (lineA, lineB) ->
+        match lineA, lineB with
+        | _ when areParallel lineA lineB -> None
+        | _ when xOutOfRange lineA lineB -> None
+        | _ -> intersect lineA lineB)
+    |> Array.filter ((<>) None)
+    |> Array.filter (fun (Some (x, y)) -> x >= 0 && x <= size && y >= 0 && y <= size)
+
+let intersectWith size sensors sensor =
+    sensors
+    |> Array.map (intersectWithSensor size sensor)
     |> Array.concat
-    |> Array.head
-    |> fun (x, y) -> x * 4000000 + y
+    |> Array.map (function
+        | Some x -> x
+        | None -> failwith "oops")
 
-let toDistance ((sx, sy), (bx, by)) =
-    (sx, sy), abs(sx-bx) + abs(sy-by)
 
-let pointsOutOfRange ((sx, sy), range) =
-    [| for dx in 0..range -> sx + dx, sy + range - dx |]
-    |> Array.append [| for dx in 0..range -> sx-dx, sy - (range - dx) |]
-    |> Array.append [| for dx in 0..range -> sx+dx, sy - (range - dx) |]
-    |> Array.append [| for dx in 0..range -> sx-dx, sy + (range - dx) |]
-    |> Array.distinct
+let findIntersections size sensors =
+    sensors |> Array.map (intersectWith size sensors) |> Array.concat
 
-let inRange (point: int * int) (sensor: int * int, range: int) =
-    snd (toDistance (sensor, point)) <= range
+let outOfRange sensors (x, y) =
+    sensors
+    |> Array.map (fun ((x1, y1), (x2, y2)) -> (x1, y1), abs (x2 - x1) + abs (y2 - y1))
+    |> Array.forall (fun ((sx, sy), r) -> abs (sx - x) + abs (sy - y) > r)
 
-let part2' size sensors =
-    let sensors' =
-        sensors
-        |> Array.map toDistance
-    sensors'
-    |> Array.map pointsOutOfRange
-    |> Array.concat
-    |> Array.filter (fun (x, y) -> x >= 0 && x <= size && y >= 0 && y <= size)
-    |> Array.filter (fun point -> sensors' |> Array.forall (inRange point >> not))
+
+let part2 size sensors =
+    sensors
+    |> Array.map toLines
+    |> findIntersections size
+    |> Array.filter (outOfRange sensors)
     |> Array.distinct
     |> Array.head
-    |> fun (x, y) -> (int64 x) * (int64 size) + (int64 y)
-
-// 2889605L*4_000_000L+3398893L
+    |> fun (x, y) -> 4_000_000L * (int64 x) + (int64 y)
 
 [<EntryPoint>]
 let main argv =
     let input = File.ReadAllLines("input.txt")
     let testInput = File.ReadAllLines("sample.txt")
 
-    testInput |> parse |> part2' 20 |> printfn "%A"
+    // testInput |> parse |> part1 10 |> printfn "%A"
+    // testInput |> parse |> part2' 20 |> printfn "%A"
 
-    // input |> parse |> part1 2000000 |> printfn "Part 1: %A"
-    input |> parse |> part2' 4_000_000 |> printfn "Part 2: %A"
+    input |> parse |> part1 2_000_000 |> printfn "Part 1: %A"
+    input |> parse |> part2 4_000_000 |> printfn "Part 2: %d"
 
     0 // return an integer exit code
