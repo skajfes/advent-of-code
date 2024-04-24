75fc859cadf91cf9b42cfb62017e7dac0247497c
Saša Kajfeš <sasa.kajfes@moberg.hr>
2024-04-24 22:45:51 +0200

    2021 Day23 Part2 solved

--- 2021/Day23/Program.fs
+++ 2021/Day23/Program.fs
@@ -19,6 +19,27 @@ let parse (input: string[]) =
     |> List.map (fun (k, v) -> KeyValuePair(k, v))
     |> FSharp.HashCollections.HashMap.ofSeq
 
+let parse2 (input: string[]) =
+    [
+        ((1, 2), input[2].[3])
+        ((2, 2), 'D')
+        ((3, 2), 'D')
+        ((4, 2), input[3].[3])
+        ((1, 4), input[2].[5])
+        ((2, 4), 'C')
+        ((3, 4), 'B')
+        ((4, 4), input[3].[5])
+        ((1, 6), input[2].[7])
+        ((2, 6), 'B')
+        ((3, 6), 'A')
+        ((4, 6), input[3].[7])
+        ((1, 8), input[2].[9])
+        ((2, 8), 'A')
+        ((3, 8), 'C')
+        ((4, 8), input[3].[9])
+    ]
+    |> List.map (fun (k, v) -> KeyValuePair(k, v))
+    |> FSharp.HashCollections.HashMap.ofSeq
 
 // let maze1 =
 //     [(0, 0), "0"; (0, 1), "1"; (0, 2), "-1"; (0, 3), "2"; (0, 4), "-2"; (0, 5), "3"; (0, 6), "-3"; (0, 7), "4"; (0, 8), "-4"; (0, 9), "5"; (0, 10), "6";
@@ -151,11 +172,63 @@ let allPossiblePositions (board: Board) struct (start, amphipod) : ((Board * int
                                      |> FSharp.HashCollections.HashMap.remove start
                                      |> FSharp.HashCollections.HashMap.add pos amphipod), score amphipod path)
 
+let allPossiblePositions2 (board: Board) struct (start, amphipod) : ((Board * int) list) =
+    let has (key, value) = board |> FSharp.HashCollections.HashMap.containsKey key && board.Item(key) = value
+    match start with
+    | x, y when x = 0 ->
+        match amphipod with
+        | 'A' when has ((2, 2), 'A') && has ((3, 2), 'A') && has ((4, 2), 'A') -> [(1, 2)]
+        | 'A' when has ((3, 2), 'A') && has ((4, 2), 'A') -> [(2, 2)]
+        | 'A' when has ((4, 2), 'A') -> [(3, 2)]
+        | 'A' -> [(4, 2)]
+        | 'B' when has ((2, 4), 'B') && has ((3, 4), 'B') && has ((4, 4), 'B') -> [(1, 4)]
+        | 'B' when has ((3, 4), 'B') && has ((4, 4), 'B') -> [(2, 4)]
+        | 'B' when has ((4, 4), 'B') -> [(3, 4)]
+        | 'B' -> [(4, 4)]
+        | 'C' when has ((2, 6), 'C') && has ((3, 6), 'C') && has ((4, 6), 'C')  -> [(1, 6)]
+        | 'C' when has ((3, 6), 'C') && has ((4, 6), 'C') -> [(2, 6)]
+        | 'C' when has ((4, 6), 'C') -> [(3, 6)]
+        | 'C' -> [(4, 6)]
+        | 'D' when has ((2, 8), 'D') && has ((3, 8), 'D') && has ((4, 8), 'D')  -> [(1, 8)]
+        | 'D' when has ((3, 8), 'D') && has ((4, 8), 'D') -> [(2, 8)]
+        | 'D' when has ((4, 8), 'D') -> [(3, 8)]
+        | 'D' -> [(4, 8)]
+        | _ -> failwith "unknown amphipod"
+    | x, y when x > 0 ->
+        match amphipod, start with
+        | 'A', (4, 2) -> []
+        | 'A', (3, 2) when has ((4, 2), 'A') -> []
+        | 'A', (2, 2) when has ((3, 2), 'A') && has ((4, 2), 'A') -> []
+        | 'A', (1, 2) when has ((2, 2), 'A') && has ((3, 2), 'A') && has ((4, 2), 'A') -> []
+        | 'A', _ -> firstRow
+        | 'B', (4, 4) -> []
+        | 'B', (3, 4) when has ((4, 4), 'B') -> []
+        | 'B', (2, 4) when has ((3, 4), 'B') && has ((4, 4), 'B') -> []
+        | 'B', (1, 4) when has ((2, 4), 'B') && has ((3, 4), 'B') && has ((4, 4), 'B') -> []
+        | 'B', _ -> firstRow
+        | 'C', (4, 6) -> []
+        | 'C', (3, 6) when has ((4, 6), 'C') -> []
+        | 'C', (2, 6) when has ((3, 6), 'C') && has ((4, 6), 'C') -> []
+        | 'C', (1, 6) when has ((2, 6), 'C') && has ((3, 6), 'C') && has ((4, 6), 'C') -> []
+        | 'C', _ -> firstRow
+        | 'D', (4, 8) -> []
+        | 'D', (3, 8) when has ((4, 8), 'D') -> []
+        | 'D', (2, 8) when has ((3, 8), 'D') && has ((4, 8), 'D') -> []
+        | 'D', (1, 8) when has ((2, 8), 'D') && has ((3, 8), 'D') && has ((4, 8), 'D') -> []
+        | 'D', _ -> firstRow
+        | _ -> failwith "no"
+    | _ -> failwith "unknown position"
+    |> List.map (fun stop -> stop, getPath' start stop)
+    |> List.filter (fun (stop, path) -> pathEmpty board path)
+    |> List.map (fun (pos, path) -> (board
+                                     |> FSharp.HashCollections.HashMap.remove start
+                                     |> FSharp.HashCollections.HashMap.add pos amphipod), score amphipod path)
+
 let nextPositions maze (board: Board): ((Board*int) list) =
     board
     |> FSharp.HashCollections.HashMap.toSeq
     |> Seq.toList
-    |> List.collect (allPossiblePositions board)
+    |> List.collect (allPossiblePositions2 board)
 
 // let goal1 = [|(7, 'A'); (8, 'A'); (9, 'B'); (10, 'B'); (11, 'C'); (12, 'C'); (13, 'D'); (14, 'D'); |]
 // let goal2 = Array.append goal1 [|(7, 'A'); (8, 'A'); (9, 'B'); (10, 'B'); (11, 'C'); (12, 'C'); (13, 'D'); (14, 'D'); |]
@@ -194,9 +267,9 @@ let shortestPath isDone (maze: (int*int)list) (board: Board) =
 
         if visited.Contains(board) then find() else
 
-        // if cost > lastCost then
-        //     printfn "%d" cost
-        //     lastCost <- cost
+        if cost > lastCost then
+            printfn "%d %d" (queue.Count) cost
+            lastCost <- cost
 
         if isDone board then cost else
         visited.Add(board) |> ignore
@@ -222,7 +295,7 @@ let main argv =
 
     // testInput |> parse |> shortestPath (isDone goal1) maze1 |> printfn "Test 1: %A"
     // input |> parse |> shortestPath (isDone goal1) maze1 |> printfn "Part 1: %A"
-    testInput |> parse |> shortestPath (isDone goal2) maze2 |> printfn "Test 2: %A"
-    // input |> parse |> part2 |> printfn "Part 2: %A"
+    // testInput |> parse2 |> shortestPath (isDone goal2) maze2 |> printfn "Test 2: %A"
+    input |> parse2 |> shortestPath (isDone goal2) maze2 |> printfn "Part2 2: %A"
 
     0 // return an integer exit code
