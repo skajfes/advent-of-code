b44afca49293d3352eec012736ddc961363e4dda
Saša Kajfeš <sasa.kajfes@moberg.hr>
2024-12-22 16:48:48 +0100

    2024 Day21

--- 2024/Day21/Program.fs
+++ 2024/Day21/Program.fs
@@ -1,3 +1,4 @@
+open System.Collections.Generic
 open System.IO
 
 let parse (input: string[]) = 
@@ -25,45 +26,6 @@ let arrowpad =
     |]
     |> to_pad
 
-let from_to pad a b =
-    let ar, ac = Map.find a pad
-    let br, bc = Map.find b pad
-    let v_diff, v_dir = if ar > br then -1, '^' else 1, 'v'
-    let h_diff, h_dir = if ac > bc then -1, '<' else 1, '>'
-    let path =
-        [for _ in ar..v_diff..br-v_diff do v_dir ]
-        |> List.append [for _ in ac..h_diff..bc-h_diff do h_dir ]
-        |> List.append ['A']
-    // path
-    let over_hole =
-        (path, (ar, ac))
-        ||> List.mapFoldBack (fun el (r, c) ->
-                match el with
-                | '^' -> (r-1, c), (r-1, c)
-                | 'v' -> (r+1, c), (r+1, c)
-                | '<' -> (r, c-1), (r, c-1)
-                | '>' -> (r, c+1), (r, c+1)
-                | _ -> (r, c), (r, c)
-            )
-        |> fst
-        |> List.map (fun c -> pad |> Map.pick (fun k v -> if v = c then Some k else None) )
-        |> List.exists (function | ' ' -> true | _ -> false)
-
-    if over_hole
-    then
-        [for _ in ac..h_diff..bc-h_diff do h_dir ]
-        |> List.append [for _ in ar..v_diff..br-v_diff do v_dir ]
-        |> List.append ['A']
-    else path
-
-
-let decode pad code =
-    code
-    |> Seq.fold (fun (pos, commands) c ->
-        let cs = from_to pad pos c
-        c, cs::commands
-    ) ('A', [])
-    |> fun (_, commands) -> commands |> Seq.rev |> Seq.concat
 
 let rec distribute e = function
   | [] -> [[e]]
@@ -73,7 +35,6 @@ let permute list =
     let rec permute' = function
       | [] -> [[]]
       | e::xs -> List.collect (distribute e) (permute' xs)
-
     permute' list
     |> List.distinct
 
@@ -91,7 +52,7 @@ let over_hole pad path (ar, ac) =
     |> List.map (fun c -> pad |> Map.pick (fun k v -> if v = c then Some k else None) )
     |> List.exists (function | ' ' -> true | _ -> false)
 
-let from_to' pad a b =
+let from_to pad a b =
     let ar, ac = Map.find a pad
     let br, bc = Map.find b pad
     let v_diff, v_dir = if ar > br then -1, '^' else 1, 'v'
@@ -105,26 +66,39 @@ let from_to' pad a b =
     |> List.filter (fun p -> not (over_hole pad p (ar, ac)))
     |> List.map (fun a -> List.append a ['A'])
 
-let decode' pad code =
+let decode pad code =
     code
     |> Seq.fold (fun (pos, commands) c ->
-        let cs = from_to' pad pos c
+        let cs = from_to pad pos c
         c, cs::commands
     ) ('A', [])
     |> fun (_, commands) -> Seq.rev commands
 
-let part1 input =
+let memo = Dictionary<int*char list, int64>()
+let rec arrowpad_decode n code: int64 =
+    if memo.ContainsKey(n, code) then
+        memo.[n, code] else
+
+    let result: int64 =
+        if n = 0 then
+            List.length code else
+        decode arrowpad code
+        |> Seq.map (Seq.map (arrowpad_decode (n-1)))
+        |> Seq.map (Seq.min)
+        |> Seq.sum
+
+    memo.Add((n, code), result)
+    result
+
+
+
+let minimal_presses robot_number input =
     input
-    |> Array.map (decode' numpad)
-    |> Array.map (Seq.map (List.map (decode' arrowpad)))
-    |> Array.map (Seq.map (List.map (Seq.map (List.map (decode' arrowpad)))))
-    |> Array.map (Seq.map (List.map (Seq.map (List.map (Seq.map (List.map List.length >> List.min))))))
-    |> Array.map (Seq.map (List.map (Seq.map (List.map (Seq.sum)))))
-    |> Array.map (Seq.map (List.map (Seq.map (List.min ))))
-    |> Array.map (Seq.map (List.map (Seq.sum )))
-    |> Array.map (Seq.map (List.min ))
-    |> Array.map (Seq.sum )
-    |> Array.mapi (fun i s -> s * (input[i] |> Seq.map string |> String.concat "" |> fun x -> x.Replace("A", "") |> int))
+    |> Array.map (decode numpad)
+    |> Array.map (Seq.map (List.map (arrowpad_decode robot_number)))
+    |> Array.map (Seq.map List.min)
+    |> Array.map Seq.sum
+    |> Array.mapi (fun i s -> s * (input[i] |> Seq.map string |> String.concat "" |> fun x -> x.Replace("A", "") |> int64))
     |> Array.sum
 
 [<EntryPoint>]
@@ -132,9 +106,9 @@ let main argv =
     let input = File.ReadAllLines("input.txt")
     let testInput = File.ReadAllLines("sample.txt")
     
-    testInput |> parse |> part1 |> printfn "%A"
-    
-    input |> parse |> part1 |> printfn "Part 1: %A"
-    // input |> parse |> part2 |> printfn "Part 2: %A"
+    testInput |> parse |> minimal_presses 2 |> printfn "%A"
+
+    input |> parse |> minimal_presses 2 |> printfn "Part 1: %d"
+    input |> parse |> minimal_presses 25 |> printfn "Part 2: %d"
 
     0 // return an integer exit code
