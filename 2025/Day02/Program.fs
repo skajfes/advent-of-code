2214acbff9a75e8a812d3e4942352a7576bd1fa4
Saša Kajfeš <sasa.kajfes@moberg.hr>
2025-12-02 23:19:40 +0100

    2025 Day02

--- 2025/Day02/Program.fs
+++ 2025/Day02/Program.fs
@@ -7,54 +7,49 @@ let parse (input: string[]) =
     |> _.Split([|','|])
     |> Array.map (_.Split([|'-'|]) >> fun [| a; b |] -> int64 a, int64 b)
 
-
-let is_valid s =
-    let digits = string s |> String.length
-    let digits2 = digits / 2
-    let d = pown 10L (digits2)
-
-    let a = s / d
-    let b = s % d
-
-    printfn "%d %d %d %d %A" s digits digits2 d (a=b)
-
-    a = b
-
+// get multiplier of n, its 10 with number of zeroes as length 72 -> 100
 let digits n =
     string n |> String.length |> pown 10L
 
-let multiplier n =
-    let digits = string n |> String.length
-    let digits2 = digits / 2
-    let rest = digits - digits2
-    pown 10L (rest)
+// construct a number by repeating digits x n times, multiplying by d everytime
+let rec construct (x: int64) d n =
+    match n with
+    | 1 -> x
+    | _ -> (construct x d (n - 1)) * d + x
 
-let valid_in_range a b =
-    let d = multiplier a
+// generate all invalid numbers where pattern repeats n times, in range start-stop
+let valid_in_range n start stop =
+    let len = string start |> String.length
+    let len' = len / n
+    let multiplier = pown 10L (len - len')
 
-    let start = a / d
-    let stop = b / d
+    let start' = start / multiplier
+    let stop' = stop / multiplier
 
     // printfn "%d %d %d %d" a d start stop
 
-    [|for i in start..stop -> i*(digits i)+i|]
-    |> Array.filter (fun x -> x >= a && x <= b)
+    [|for i in start'..stop' -> construct i (digits i) n |]
+    |> Array.filter (fun x -> x >= start && x <= stop)
 
-let valid_in_ranges a b =
-    valid_in_range
+// generate all invalid numbers where pattern repeats 2 to length of stop times
+let valid_in_ranges start stop =
+    let len = string stop |> String.length
+    [|2..len|]
+    |> Array.map (fun n -> valid_in_range n start stop)
+    |> Array.concat
+    |> Array.distinct
+    |> Array.sum
 
 let part1 ranges =
     ranges
-    |> Array.map (fun (a, b) -> valid_in_range a b)
+    |> Array.map (fun (a, b) -> valid_in_range 2 a b)
     |> Array.concat
     |> Array.sum
-
 
 let part2 ranges =
     ranges
     |> Array.map (fun (a, b) -> valid_in_ranges a b)
-
-
+    |> Array.sum
 
 
 [<EntryPoint>]
@@ -63,9 +58,9 @@ let main argv =
     let testInput = File.ReadAllLines("sample.txt")
     
     // testInput |> parse |> part1 |> printfn "%A"
-    testInput |> parse |> part2 |> printfn "%A"
+    // testInput |> parse |> part2 |> printfn "%A"
 
-    // input |> parse |> part1 |> printfn "Part 1: %d"
-    // input |> parse |> part2 |> printfn "Part 2: %A"
+    input |> parse |> part1 |> printfn "Part 1: %d"
+    input |> parse |> part2 |> printfn "Part 2: %d"
 
     0 // return an integer exit code
